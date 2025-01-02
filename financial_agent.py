7b19d476496fd9b68593c8992b9f7e2cc3b19d3e
Jai Vadula <jaivadula@gmail.com>
2025-01-02 21:25:49 +0530

    financialagent.py

--- financial_agent.py
+++ financial_agent.py
@@ -13,3 +13,21 @@ web_search_agent= Agent(
     markdown=True,
 )
 
+financial_agent=Agent(
+    name="Financial Agent",
+    role="get financial information",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[YFinanceTools(stock_fundamentals=True, analyst_recommendations=True, stock_price=True, company_news=True)],
+    instructions=["Use tables to display the data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+multi_ai_agent=Agent(
+    team=[web_search_agent,financial_agent],
+    instructions=["Always include sources", "Use table to display the data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+multi_ai_agent.print_response("Summarize analyst recommendation and share the latest news for NVIDIA",stream=True)
