1ae2d69e36182011b05e0a695c50c65cc504ddf7
Jai Vadula <jaivadula@gmail.com>
2025-01-02 21:07:27 +0530

    FinancialAgent.py

--- financial_agent.py
+++ financial_agent.py
@@ -1,3 +1,15 @@
-from phi.agent import agent
+from phi.agent import Agent
 from phi.model.groq import Groq
+from phi.tools.duckduckgo import DuckDuckGo
 from phi.tools.yfinance import YFinanceTools
+
+web_search_agent= Agent(
+    name="Web Search Agent",
+    role="search the web for the information",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include sources"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
